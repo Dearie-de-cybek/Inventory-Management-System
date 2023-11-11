9542582189a88c4fd2e84e8ec5405871ac756e3b
Dearie <eburujacintha@gmail.com>
2023-11-11 02:50:10 +0100

    Added initial sidebar structure

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+const path = require('path');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 app.set('view engine', 'ejs');
-app.set('views', (`${__dirname}/views`));
-
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', path.join(`${__dirname}/views`));
 
 
 
